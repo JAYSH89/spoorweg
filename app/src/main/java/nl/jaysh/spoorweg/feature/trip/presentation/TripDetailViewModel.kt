5730d4f86d9ddd6fb6a6ba62a973d726e32fccef
Jay <sanjaysheombar@gmail.com>
2024-07-08 23:14:39 +0200

    [SP-01] Add @HiltViewModel to viewModels

--- app/src/main/java/nl/jaysh/spoorweg/feature/trip/presentation/TripDetailViewModel.kt
+++ app/src/main/java/nl/jaysh/spoorweg/feature/trip/presentation/TripDetailViewModel.kt
@@ -1,10 +1,12 @@
 package nl.jaysh.spoorweg.feature.trip.presentation
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
+@HiltViewModel
 class TripDetailViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow(TripDetailState())
     val state: StateFlow<TripDetailState> = _state
